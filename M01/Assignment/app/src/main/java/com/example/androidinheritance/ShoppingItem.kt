51931d86a1c8eb20f654a34d96ef9a14adf3b3db
jDanielDetes <jdanieldetes@gmail.com>
2019-09-09 16:38:33 -0400

    added subclasses and added twist to things to display

--- M01/Assignment/app/src/main/java/com/example/androidinheritance/ShoppingItem.kt
+++ M01/Assignment/app/src/main/java/com/example/androidinheritance/ShoppingItem.kt
@@ -1,11 +1,32 @@
 package com.example.androidinheritance
 
-class ShoppingItem (val colorId : Int , val productName : String) {
+open class ShoppingItem (val colorId : Int , val productName : String) {
 
-    fun getdisplayName() : String {
+   open fun getdisplayName() : String {
         return "The product is a $productName"
     }
 
-    class Apple(colorId: Int=1, productName: String)
+    class fruitItem (colorId: Int, productName: String) : ShoppingItem(colorId,productName){
+        override  fun getdisplayName():String {
+            return "It's a $productName and the color is $colorId great for snacking and must be refrigerated "
+        }
+    }
+
+    class winterClothes (colorId: Int,productName: String) : ShoppingItem(colorId,productName) {
+        override fun getdisplayName(): String {
+            return "Its a $productName great for cold weather the color is $colorId size is medium"
+        }
+
+    }
+
+
+    class newCar (colorId: Int,productName: String): ShoppingItem(colorId,productName) {
+        override fun getdisplayName(): String {
+            return "Its a brand new $productName pretty expensive but the color $colorId looks great "
+        }
+    }
+
+
+
 
 }
