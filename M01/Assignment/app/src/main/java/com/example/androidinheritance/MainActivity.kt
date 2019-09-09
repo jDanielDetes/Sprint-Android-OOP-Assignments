4479b1e010ab1d1ff79a96f36a52607790de370d
jDanielDetes <jdanieldetes@gmail.com>
2019-09-09 17:53:00 -0400

    Generated test values and made a list

--- M01/Assignment/app/src/main/java/com/example/androidinheritance/MainActivity.kt
+++ M01/Assignment/app/src/main/java/com/example/androidinheritance/MainActivity.kt
@@ -1,12 +1,32 @@
 package com.example.androidinheritance
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    val shoppingList = ArrayList<ShoppingItem>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        generateTestValues()
+    }
+
+        val testValues: ArrayList<ShoppingItem> = arrayListOf(
+
+            ShoppingItem.fruitItem(Color.RED,"Apple",true),
+            ShoppingItem.fruitItem(Color.YELLOW,"Banana",true),
+            ShoppingItem.winterClothes(Color.GREEN,"Hoodie","Medium"),
+            ShoppingItem.winterClothes(Color.LTGRAY,"Jacket","Large"),
+            ShoppingItem.newCar(Color.BLUE,"Car","Tesla"),
+            ShoppingItem.newCar(Color.WHITE,"Car","Toyota")
+
+        )
+    fun generateTestValues() {
+        testValues.forEach {
+            shoppingList.add(it)
+        }
     }
 }
--- M01/Assignment/app/src/main/java/com/example/androidinheritance/ShoppingItem.kt
+++ M01/Assignment/app/src/main/java/com/example/androidinheritance/ShoppingItem.kt
@@ -6,23 +6,24 @@ open class ShoppingItem (val colorId : Int , val productName : String) {
         return "The product is a $productName"
     }
 
-    class fruitItem (colorId: Int, productName: String) : ShoppingItem(colorId,productName){
+    class fruitItem (colorId: Int, productName: String, val isRefrigerated: Boolean) : ShoppingItem(colorId,productName){
         override  fun getdisplayName():String {
-            return "It's a $productName and the color is $colorId great for snacking and must be refrigerated "
+            return "It's a $productName and the color is $colorId great for snacking but does this item need to be  refrigerated true or false? " +
+                    ".. well obviously it is $isRefrigerated "
         }
     }
 
-    class winterClothes (colorId: Int,productName: String) : ShoppingItem(colorId,productName) {
+    class winterClothes (colorId: Int,productName: String, val size : String) : ShoppingItem(colorId,productName) {
         override fun getdisplayName(): String {
-            return "Its a $productName great for cold weather the color is $colorId size is medium"
+            return "Its a $productName great for cold weather the color is $colorId size is $size"
         }
 
     }
 
 
-    class newCar (colorId: Int,productName: String): ShoppingItem(colorId,productName) {
+    class newCar (colorId: Int,productName: String, val carBrand: String): ShoppingItem(colorId,productName) {
         override fun getdisplayName(): String {
-            return "Its a brand new $productName pretty expensive but the color $colorId looks great "
+            return "Its a brand new $productName pretty expensive but the color $colorId looks great not to mention it is a $carBrand "
         }
     }
 
