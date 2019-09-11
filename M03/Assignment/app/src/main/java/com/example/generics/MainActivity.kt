fd99066238f8204d9a595297cce3a818895f1579
jDanielDetes <jdanieldetes@gmail.com>
2019-09-11 16:14:25 -0400

    fixed Task1

--- M03/Assignment/app/src/main/java/com/example/generics/MainActivity.kt
+++ M03/Assignment/app/src/main/java/com/example/generics/MainActivity.kt
@@ -8,11 +8,16 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val Task1  = mutableListOf("test1", "test2","test3")
+        Task1.add("test4")
+        Task1.add(5.toString())
     }
 
-    val test = mutableListOf<String>(
-        "gfgg","super test", 4,"five"
-    )
 
 
+class task2(): String {
+
+
+}
 }
